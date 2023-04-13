adeb76ca5bbd5ee8743c8c19ef8ed4d16af2f5c5
Uriel B <user@Users-MacBook-Pro.local>
2023-04-13 02:26:08 -0600

    Agregando driving

--- src/app/maps/driving/components/driving.component.ts
+++ src/app/maps/driving/components/driving.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { DialogModule } from 'primeng/dialog';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-driving',
   templateUrl: './driving.component.html',
+  styleUrls: ['./driving.component.css']
 })
 export class DrivingComponent implements OnInit {
+  @Input() engineOnText: {on: string, off: string} = {on: 'Apagar Auto', off: 'Encender Auto'};
+  @Input() doorOpenText: {open: string, close: string} = {open: 'Abrir Auto', close: 'Cerrar Auto'};
+
+  engineOn: boolean = false;
+  doorOpen: boolean = false;
+
+  @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef;
 
   constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  toggleEngine(): void {
+    this.engineOn = !this.engineOn;
   }
 
+  toggleDoor(): void {
+    this.doorOpen = !this.doorOpen;
+  }
+
+  playVideo(): void {
+    const player = this.videoPlayer.nativeElement as HTMLVideoElement;
+    player.currentTime = 222;
+    player.play();
+  }
 }
